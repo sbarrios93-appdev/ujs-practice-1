3c130512f158117b01e058ded9fb9264403b7bc1
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2023-01-17 20:26:25 +0000

    Add tasks#move action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,19 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :set_task, only: %i[ show edit update destroy move]
+
+  def move
+    if @task.not_yet_started?
+      @task.in_progress!
+    elsif @task.in_progress?
+      @task.completed!
+    else
+      @task.in_progress!
+    end
+
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: "Task updated" }
+    end
+  end
 
   # GET /tasks or /tasks.json
   def index
